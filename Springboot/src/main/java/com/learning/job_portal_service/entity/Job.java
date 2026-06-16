094b4ca4e54fce45ad57577b8022c36108cba47a
Manohar B <manohar1ga15ec430@gmail.com>
2026-06-16 14:04:19 +0530

    Feature/completes job model | database connection | Flyway migration file | DTO's

---
+++ Springboot/src/main/java/com/learning/job_portal_service/entity/Job.java
@@ -0,0 +1,51 @@
+package com.learning.job_portal_service.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.learning.job_portal_service.enums.JobStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "job")
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length= 2000)
+    private String description;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private JobStatus status;
+
+    @Column(nullable = false)
+    private BigDecimal SalaryMin;
+
+    @Column(nullable = false)
+    private BigDecimal SalaryMax;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
