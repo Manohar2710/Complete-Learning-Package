97a6415106647609d91439d96328d9cab1416f23
Manohar B <manohar1ga15ec430@gmail.com>
2026-07-01 15:59:01 +0530

    Feature/springboot/143/Implement UserReposity with findByEmail method

---
+++ Springboot/security-module/src/main/java/com/learning/security/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.learning.security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.learning.security.entity.User;
+import java.util.Optional;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+}
